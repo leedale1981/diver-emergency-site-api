a1332319a6b1a36a1d001d3174567a9481bce7d8
Lee Dale <lee.jdale@gmail.com>
2021-10-02 18:00:00 +0100

    Update

--- controllers/sites.go
+++ controllers/sites.go
@@ -7,8 +7,13 @@ type SitesController struct {
 
 func (sitesController SitesController) registerRoutes() {
 	http.HandleFunc("/sites", sitesController.handleSites)
+	http.HandleFunc("/sites/nearest/", sitesController.handleNearestSite)
 }
 
 func (sitesController SitesController) handleSites(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello sites!"))
+}
+
+func (sitesController SitesController) handleNearestSite(w http.ResponseWriter, r *http.Request) {
+
 }
--- models/chamber.go
+++ models/chamber.go
@@ -6,6 +6,34 @@ type Chamber struct {
 	AddressLine1 string
 	AddressLine2 string
 	PostCode     string
-	LatLong      string
+	Lat          float32
+	Long         float32
 	PhoneNumber  string
+}
+
+func (chamber Chamber) GetMockChambers() []Chamber {
+	var chambers []Chamber = []Chamber{
+		{
+			Id:           1,
+			Name:         "Test Chaber 1",
+			AddressLine1: "Test Line 1",
+			AddressLine2: "Test Line 2",
+			PostCode:     "AB1 2CD",
+			Lat:          50.1,
+			Long:         45.2,
+			PhoneNumber:  "0800908765",
+		},
+		{
+			Id:           2,
+			Name:         "Test Chaber 1",
+			AddressLine1: "Test Line 1",
+			AddressLine2: "Test Line 2",
+			PostCode:     "CD2 2EF",
+			Lat:          51.1,
+			Long:         34.1,
+			PhoneNumber:  "0800909778",
+		},
+	}
+
+	return chambers
 }
